09e5fbd913db384e567d6f1ce002dd67c55c57fe
changhoon-kim <changhoon.kim@worksmobile.com>
2022-02-14 20:27:43 +0900

    blog post list page using CBV

--- ch2/blog/views.py
+++ ch2/blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # NOTE: template_name 을 직접 지정
+    # template_name = 'blog/index.html'
+
+'''
+FBV 방식
 def index(request):
     posts = Post.objects.all().order_by('-pk')
 
@@ -11,6 +20,7 @@ def index(request):
             'posts': posts,
         }
     )
+'''
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
