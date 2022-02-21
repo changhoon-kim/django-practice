0afacb1fbcd7ac8997f3ec121680dd743610fd49
changhoon-kim <changhoon.kim@worksmobile.com>
2022-02-21 17:48:59 +0900

    make category page

--- ch2/blog/views.py
+++ ch2/blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from .models import Post, Category
 
 # NOTE: blog/templates/blog/post_list.html 찾음
@@ -23,6 +24,25 @@ class PostDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
 
 '''
 FBV 방식
