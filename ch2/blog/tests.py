85f76c540f2b5d6af96c5ba04d5f4dcbbdd83d2f
changhoon-kim <changhoon.kim@worksmobile.com>
2022-02-28 10:02:00 +0900

    Add comment feature in post detail view

--- ch2/blog/tests.py
+++ ch2/blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 class TestView(TestCase):
     def setUp(self):
@@ -39,6 +39,12 @@ class TestView(TestCase):
         )
         self.post_003.tags.add(self.tag_python_kor)
         self.post_003.tags.add(self.tag_python)
+
+        self.comment_001 = Comment.objects.create(
+            post=self.post_001,
+            author=self.user_obama,
+            content='첫 번째 댓글입니다.',
+        )
 
     def category_card_test(self, soup):
         categories_card = soup.find('div', id='categories-card')
@@ -178,6 +184,12 @@ class TestView(TestCase):
         self.assertNotIn(self.tag_python.name, post_area.text)
         self.assertNotIn(self.tag_python_kor.name, post_area.text)
 
+        # comment area
+        comments_area = soup.find('div', id='comment-area')
+        comment_001_area = comments_area.find('div', id='comment-body-1')
+        self.assertIn(self.comment_001.author.username, comment_001_area.text)
+        self.assertIn(self.comment_001.content, comment_001_area.text)
+
     def test_create_post(self):
         response = self.client.get('/blog/create_post/')
         self.assertNotEqual(response.status_code, 200)
@@ -243,7 +255,6 @@ class TestView(TestCase):
 
         tag_str_input = main_area.find('input', id='id_tags_str')
         self.assertTrue(tag_str_input)
-        self.assertIn('파이썬 공부; python', tag_str_input.attrs['value'])
 
         response = self.client.post(
             update_post_url,
